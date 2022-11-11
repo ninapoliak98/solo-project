4721364a8e6c7a367b6fffa25e945c5484502bc8
Nina Poliak-Karatkevich <ninapoliakkarat@gmail.com>
2022-11-11 11:16:36 -0500

    changed something in nav bar

--- client/my-app/src/App/CryptoGraph.jsx
+++ client/my-app/src/App/CryptoGraph.jsx
@@ -10,21 +10,27 @@ import {
     ResponsiveContainer,
 } from 'recharts'
 
-
-
 export default function CryptoGraph({data}) {
+    //format data into something like this
+    // const data = [
+    //     {
+    //         "name": "Page A",
+    //         "uv": 4000,
+    //         "pv": 2400,
+    //         "amt": 2400
+    //     },
+    console.log(data.prices)
 
     return(
         <div>
-            <LineChart width={730} height={250} data={data.prices}
+            <LineChart width={730} height={250} data={data}
                        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
                 <YAxis />
                 <Tooltip />
                 <Legend />
-                <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                <Line type="monotone" stroke="#8884d8" />
             </LineChart>
         </div>
     )
